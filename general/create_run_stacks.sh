9361c23e7712cacd565f0c0181cc9cef4cce14be
Adis <hllvc@icloud.com>
2023-02-23 17:36:15 +0100

    add sh script to support run-stack

---
+++ general/create_run_stacks.sh
@@ -0,0 +1,171 @@
+#!/bin/sh
+
+# Define variables
+readonly base_url="${SG_BASE_URL:-"https://api.app.stackguardian.io"}"
+readonly api_url="$base_url/api/v1"
+readonly dashboard_url="${SG_DASHBOARD_URL:-"https://app.stackguardian.io/orchestrator"}"
+readonly api_token="$SG_API_TOKEN"
+
+help() {
+  cat <<EOF
+
+  $(basename "$0") --org <ORG_NAME> --workflow_group <WF_GROUP_NAME> -- <JSON_PAYLOAD_PATH>
+EOF
+}
+
+if [ $# -lt 6 ]; then
+  help
+  exit 1
+fi
+
+# Parse command-line arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --org)
+            readonly org="$2"
+            shift 2
+            ;;
+        --workflow-group)
+            readonly workflow_group="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            readonly payload="$1"
+            break
+            ;;
+        *)
+            echo "Error: unknown option $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$api_token" ] || [ "sgu_" != "${api_token:0:4}" ]; then
+    echo "Invalid or no API Token provided. Expecting it in \"SG_API_TOKEN\" environment variable. Navigate to StackGuardian platform to get your api token: $dashboard_url/orgs/$org/settings?tab=api_key"
+    exit 1
+fi
+
+create_stack() {
+    org_id=$1
+    wfgrp_id=$2
+    runOnCreate=${3:-true}
+    url="$api_url/orgs/$org_id/wfgrps/$wfgrp_id/stacks/?runOnCreate=$runOnCreate"
+    headers=$(cat <<EOF
+PrincipalId: ""
+Authorization: apikey $api_token
+Content-Type: application/json
+EOF
+)
+    response=$(curl -s --http1.1 -X POST -H "$headers" -d @"$payload" "$url")
+    if [ $? -ne 0 ] || echo "$response" | grep -q "\"error\""; then
+        echo "== Stack creation failed =="
+        echo "url: $url"
+        echo "response: $response"
+        return 1
+    fi
+    echo "$response"
+}
+
+get_wfruns_in_stackrun() {
+    org_id=$1
+    wfgrp_id=$2
+    stack_id=$3
+    stackrun_id=$4
+    url="$api_url/orgs/$org_id/wfgrps/$wfgrp_id/stacks/$stack_id/$stackrun_id"
+    headers=$(cat <<EOF
+PrincipalId: ""
+Authorization: apikey $api_token
+Content-Type: application/json
+EOF
+)
+    response=$(curl -s --http1.1 -X GET -H "$headers" "$url")
+    if [ $? -ne 0 ] || echo "$response" | grep -q "\"error\""; then
+        echo "== Retrieving Workflow Run from StackRun failed =="
+        echo "url: $url"
+        echo "response: $response"
+        return 1
+    fi
+    echo "$response"
+}
+
+get_stack() {
+    org_id=$1
+    wfgrp_id=$2
+    stack_id=$3
+    url="$api_url/orgs/$org_id/wfgrps/$wfgrp_id/stacks/$stack_id"
+    headers=$(cat <<EOF
+PrincipalId: ""
+Authorization: apikey $api_token
+Content-Type: application/json
+EOF
+)
+    response=$(curl -s --http1.1 -X GET -H "$headers" "$url")
+    if [ $? -ne 0 ] || echo "$response" | grep -q "\"error\""; then
+        echo "== Retrieving Stack failed =="
+        echo "url: $url"
+        echo "response: $response"
+        return 1
+    fi
+    echo "$response"
+}
+
+get_stack_status() {
+  org_id=$1
+  wfgrp_id=$2
+  stack_id=$3
+  response=$(get_stack "$org_id" "$wfgrp_id" "$stack_id")
+  response=$(echo "$response" | jq)
+  if echo "$response" | grep -q '"msg":' && echo "$response" | grep -q '"LatestWfStatus":'; then
+    echo "$response" | jq -r '.msg.LatestWfStatus'
+  else
+    echo false
+  fi
+}
+
+get_stackrun_status() {
+  org_id=$1
+  wfgrp_id=$2
+  stack_id=$3
+  stackrun_id=$4
+  response=$(get_wfruns_in_stackrun "$org_id" "$wfgrp_id" "$stack_id" "$stackrun_id")
+  response=$(echo "$response" | jq)
+  if echo "$response" | grep -q '"LatestStatus":'; then
+    echo "$response" | jq -r '.msg.LatestStatus'
+  else
+    echo false
+  fi
+}
+
+# main function
+main() {
+    # create a stack
+    org_id=$org
+    wfgrp_id="$workflow_group"
+    response=$(create_stack "$org_id" "$wfgrp_id")
+    echo "$response"
+    if [ "$response" != "" ]; then
+        stack_id=$(echo "$response" | jq -r '.data.stack.ResourceName')
+        stack_run_id=$(echo "$response" | jq -r '.data.stack.StackRunId')
+        echo "Stack created"
+        echo "https://$base_url/orgs/$org_id/wfgrps/$wfgrp_id/stacks/$stack_id"
+    else
+        exit 1
+    fi
+
+    # check stackrun status
+    while [ "$(get_stackrun_status "$org_id" "$wfgrp_id" "$stack_id" "$stack_run_id")" != "ERRORED" ] \
+        && [ "$(get_stackrun_status "$org_id" "$wfgrp_id" "$stack_id" "$stack_run_id")" != "COMPLETED" ] \
+        && [ "$(get_stackrun_status "$org_id" "$wfgrp_id" "$stack_id" "$stack_run_id")" != "APPROVAL_REQUIRED" ]; do
+        echo "Stack under deployment"
+        sleep 5
+    done
+
+    # print final stack status
+    echo "Stack finished with $(get_stack_status "$org_id" "$wfgrp_id" "$stack_id") status"
+    exit 0
+}
+
+# run main function
+main "$@"
+
